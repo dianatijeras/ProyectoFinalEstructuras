b92a526e79c7b2ac6c4dba1bf8d4ebc625904552
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2026-05-25 02:37:20 -0500

    feat: creacion enums tiponotif y tipoticket

---
+++ backendTechPark/src/main/java/techpark/enums/TipoNotif.java
@@ -0,0 +1,14 @@
+package techpark.enums;
+
+/**
+ * Enum para los tipos de notificaciones que se pueden enviar a los usuarios.
+ */
+public enum TipoNotif {
+    SHOW,
+    CLIMA,
+    MANTENIMIENTO,
+    ESTADO_ATRACCION,
+    INCIDENTE,
+    COLA;
+}
+
---
+++ backendTechPark/src/main/java/techpark/enums/TipoTicket.java
@@ -0,0 +1,11 @@
+package techpark.enums;
+
+/**
+ * Enum para representar los tipos de tickets disponibles en el parque de diversiones.
+ */
+public enum TipoTicket {
+    GENERAL,
+    FAMILIAR,
+    FAST_PASS;
+}
+
