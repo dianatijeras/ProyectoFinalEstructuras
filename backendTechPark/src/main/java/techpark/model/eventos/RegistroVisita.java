5bc63a063db3b83e8c928a5d433598740ff965ff
juans9432 <juanj.suarezm@uqvirtual.edu.co>
2026-05-25 01:53:14 -0500

    feat: creacion de la clase RegistroVisita dentro del directorio eventos

---
+++ backendTechPark/src/main/java/techpark/model/eventos/RegistroVisita.java
@@ -0,0 +1,77 @@
+package techpark.model.eventos;
+
+import java.time.LocalDateTime;
+
+/**
+ * Registro de una visita a una atracción, con detalles como la atracción visitada, la fecha y hora de la visita, el tipo de ticket utilizado y el costo deducido.
+ */
+public class RegistroVisita {
+    private Atraccion atraccion;
+    private LocalDateTime fechaHora;
+    private TipoTicket tipoTicket;
+    private double costoDeducido;
+
+    /**
+     * Constructor de la clase RegistroVisita. Recibe una atracción, un tipo de ticket y un costo deducido, y establece la fecha y hora de la visita como el momento actual.
+     * @param atraccion
+     * @param tipoTicket
+     * @param costoDeducido
+     */
+    public RegistroVisita(Atraccion atraccion, TipoTicket tipoTicket, double costoDeducido) {
+        this(atraccion, tipoTicket, costoDeducido, LocalDateTime.now());
+    }
+
+    /**
+     * Constructor de la clase RegistroVisita. Recibe una atracción, un tipo de ticket, un costo deducido y una fecha y hora específica para la visita.
+     * @param atraccion
+     * @param tipoTicket
+     * @param costoDeducido
+     * @param fechaHora
+     */
+    public RegistroVisita(Atraccion atraccion, TipoTicket tipoTicket, double costoDeducido, LocalDateTime fechaHora) {
+        this.atraccion = atraccion;
+        this.tipoTicket = tipoTicket;
+        this.costoDeducido = costoDeducido;
+        this.fechaHora = fechaHora == null ? LocalDateTime.now() : fechaHora;
+    }
+
+    /**
+     * Método getter para obtener la atracción visitada.
+     * @return
+     */
+    public Atraccion getAtraccion() {
+        return atraccion;
+    }
+
+    /**
+     * Método getter para obtener la fecha y hora de la visita.
+     * @return
+     */
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    /**
+     * Método getter para obtener el tipo de ticket utilizado en la visita.
+     * @return
+     */
+    public TipoTicket getTipoTicket() {
+        return tipoTicket;
+    }
+
+    /**
+     * Método getter para obtener el costo deducido por la visita a la atracción.
+     * @return
+     */
+    public double getCostoDeducido() {
+        return costoDeducido;
+    }
+
+    /**
+     * Método toString para representar el registro de visita como una cadena de texto, mostrando la
+     */
+    public String toString() {
+        return fechaHora + " - " + atraccion.getNombre() + " - " + tipoTicket;
+    }
+}
+
